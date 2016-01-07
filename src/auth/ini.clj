9457bcc1986d3faa70d7ceb59b653b4291d2ba2b
Peter Romfeld & Ayaz Maroof <peter+ayaz@exiconglobal.com>
2016-01-07 16:39:18 +0800

    Dont use read-sting, just dont

--- src/auth/ini.clj
+++ src/auth/ini.clj
@@ -18,9 +18,9 @@
   (let [section (atom :default)]
     (reduce
       (fn [ret element]
-        (if (vector? (read-string element))
+        (if (.startsWith element "[")
           (do
-            (reset! section (-> element read-string first ->kebab-case-keyword))
+            (reset! section (->> element (re-find #"^\s*\[(.*?)\]") second ->kebab-case-keyword))
             ret)
           (let [[k v] (parse-item element)]
             (assoc-in ret [@section (->kebab-case-keyword k)] (str v)))))
